97b4001fd1e309ded4a00aa64a27d6c5a6f7421c
Vinícius Romão <viniciusromao.contato@gmail.com>
2020-07-31 01:03:39 -0300

    Improve turns loggin add log for other actions

--- fight-monster-game/app.js
+++ fight-monster-game/app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function () {
       var damage = this.calculateDamage(3, 10);
@@ -30,7 +31,13 @@ new Vue({
       this.checkWin();
     },
     specialAttack: function () {
-      this.monsterHealth -= this.calculateDamage(10, 20);
+      var damage = this.calculateDamage(10, 20);
+      this.monsterHealth -= damage;
+
+      this.turns.unshift({ 
+        isPlayer: true,
+        text: 'Player hits Monster hard for' + damage
+      });
 
       if (this.checkWin()) {
         return;
@@ -44,6 +51,12 @@ new Vue({
       } else {
         this.playerHealth = 100;
       }
+
+      this.turns.unshift({ 
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
+
       this.monsterAttacks();
     },
     giveUp: function () {
